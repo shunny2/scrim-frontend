aa15b959b730381ae5734d556fe0f4f8bfd4a30c
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-15 11:55:58 -0300

    Fix: Routes component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from '../pages/Home';
-import Form from '../pages/Form';
+import Form from '../pages/Register';
+import ViewMore from '../pages/ViewMore';
+import Edit from '../pages/Edit';
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/form" element={<Form />}></Route>
-            <Route path="*" element={<Navigate to="/" />}></Route>
+            <Route path="/" element={<Home />} exact/>
+            <Route path="/form" element={<Form />} />
+            <Route path="/view-more/:id" element={<ViewMore />} />
+            <Route path="/form/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 }
