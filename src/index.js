7616c8513d2a9863bef60eadc78663e1d938bef7
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-15 14:04:52 -0300

    Fix: Added queryClientProvider in application

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './services/queryClient';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
