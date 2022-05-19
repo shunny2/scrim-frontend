707c520e2cb5052bebae93b14e641cf798eff051
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-19 17:21:28 -0300

    Fix: Added new routes Login and Register

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,24 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from '../pages/Home';
-import Form from '../pages/Register';
+import Register from '../pages/Register';
 import ViewMore from '../pages/ViewMore';
 import Edit from '../pages/Edit';
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+
+const Private = ({ Item }) => {
+    const signed = true;
+    return signed > 0 ? <Item /> : <SignIn />;
+};
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} exact/>
-            <Route path="/form" element={<Form />} />
+            <Route path="/" element={<SignIn />} exact/>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<Private Item={Home} />} />
+            <Route path="/form" element={<Register />} />
             <Route path="/view-more/:id" element={<ViewMore />} />
             <Route path="/form/edit/:id" element={<Edit />} />
             <Route path="*" element={<Navigate to="/" />} />
