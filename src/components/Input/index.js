73e6d2f4671459331836e7016febad1de70f6a01
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-19 22:20:04 -0300

    Fix: Fixing input refs

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import * as S from './styles';
 
-const Input = ({ type, name, placeholder, value, onChange }) => {
+const Input = ((props, ref) => {
     return (
         <S.Input
-            type={type}
-            name={name}
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
+            type={props.type}
+            name={props.name}
+            placeholder={props.placeholder}
+            {...ref}
         />
     );
-}
+})
 
-export default Input;
+export default forwardRef(Input);
