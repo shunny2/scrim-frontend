ee49649e37281626a739ee7a5fe36ce34fd23820
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-21 16:18:47 -0300

    Fix: Moving validateToken function to inside effect

--- src/contexts/Auth/AuthProvider.js
+++ src/contexts/Auth/AuthProvider.js
@@ -6,46 +6,46 @@ export const AuthProvider = (props) => {
     const [user, setUser] = useState(null);
 
     const api = useApi();
-    
+
     useEffect(() => {
+        const validateToken = async () => {
+            const storageData = localStorage.getItem('authToken');
+            console.log(storageData);
+            if (storageData) {
+                const { user } = await api.validateToken(storageData);
+
+                if (user)
+                    setUser(user);
+            }
+        }
         validateToken();
+
     }, []);
-    
+
     const signIn = async (email, password) => {
         const { user, authorisation: { token } } = await api.signIn(email, password);
-        
+
         if (user && token) {
             setUser(user);
             setToken(token);
             return true;
         }
-        
+
         return false;
     }
-    
+
     const signOut = async () => {
         const storageData = localStorage.getItem('authToken');
-        
+
         await api.logout(storageData);
         setUser(null);
         setToken('');
     }
-    
+
     const setToken = (token) => {
         localStorage.setItem('authToken', token);
     }
-    
-    const validateToken = async () => {
-        const storageData = localStorage.getItem('authToken');
 
-        if (storageData) {
-            const { user } = await api.validateToken(storageData);
-
-            if (user)
-                setUser(user);
-        }
-    }
-      
     return (
         <AuthContext.Provider value={{ user, signIn, signOut }}>
             {props.children}
