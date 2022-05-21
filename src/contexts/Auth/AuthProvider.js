1a85bda1e01450fdfda0f634cdcf43f266a99161
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-21 13:07:36 -0300

    Fix: Fixing validateToken function in useEffect

--- src/contexts/Auth/AuthProvider.js
+++ src/contexts/Auth/AuthProvider.js
@@ -8,46 +8,44 @@ export const AuthProvider = (props) => {
     const api = useApi();
     
     useEffect(() => {
-        const validateToken = async () => {
-            const storageData = localStorage.getItem('authToken');
-    
-            if (storageData) {
-                const { user } = await api.validateToken(storageData);
-    
-                if (user)
-                    setUser(user);
-            }
-        }
-
         validateToken();
-    }, [api]);
-
+    }, []);
+    
     const signIn = async (email, password) => {
         const { user, authorisation: { token } } = await api.signIn(email, password);
-
+        
         if (user && token) {
             setUser(user);
             setToken(token);
             return true;
         }
-
+        
         return false;
     }
-
+    
     const signOut = async () => {
         const storageData = localStorage.getItem('authToken');
-
+        
         await api.logout(storageData);
         setUser(null);
         setToken('');
     }
-
+    
     const setToken = (token) => {
         localStorage.setItem('authToken', token);
     }
+    
+    const validateToken = async () => {
+        const storageData = localStorage.getItem('authToken');
 
-  
+        if (storageData) {
+            const { user } = await api.validateToken(storageData);
 
+            if (user)
+                setUser(user);
+        }
+    }
+      
     return (
         <AuthContext.Provider value={{ user, signIn, signOut }}>
             {props.children}
