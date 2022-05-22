a6fd22932215ff5c26a3635761e7e17ad8fec13c
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-22 15:23:28 -0300

    Fix: Added axios interceptor request in project

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,19 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://localhost:8000/api/',
-    timeout: 1000
+    timeout: 10000,
+});
+
+instance.interceptors.request.use(async (config) => {
+    try {
+        const oldToken = localStorage.getItem('authToken');
+        if(oldToken) {
+            config.headers.Authorization = `Bearer ${oldToken}`; 
+        }
+        return config;
+    } catch (error) {
+        console.log('Entrou error: ', error);
+    }
 });
 
 export default instance;
