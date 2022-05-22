fb5d5421cc53067008f26318ff930a8922ac872f
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-21 22:19:17 -0300

    Feat: Added new component footer in project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Footer() {
+
+    return (
+        <S.Footer>
+            <S.Container>
+                <S.Logo>
+                    <S.H1>Scrim</S.H1>
+                </S.Logo>
+            </S.Container>
+        </S.Footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+    border: 3px solid #29EBAA;
+    margin-top: 4rem;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 1100px;
+    margin: 0 auto;
+    height: 100px;
+`;
+
+export const Logo = styled.div`
+    margin: 10px;
+    font-family: monospace;
+    font-weight: 700;
+    color: #29EBAA;
+    cursor: pointer;
+`;
+
+export const H1 = styled.h1`
+    font-size: 28px;
+`;
