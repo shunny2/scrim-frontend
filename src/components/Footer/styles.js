88d1051f2a62f7559f380108e98417f34470e6b5
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-23 12:44:03 -0300

    Fix: Fixing component-styled in Footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,7 +4,7 @@ export const Footer = styled.footer`
     border: 3px solid #29EBAA;
     margin-top: 4rem;
     width: 100%;
-    position: absolute;
+    position: inherit;
     bottom: 0;
     left: 0;
 `;
