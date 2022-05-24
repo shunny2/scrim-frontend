2e61704beb63b3e38c5340076d1724d4e00df619
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-24 14:02:21 -0300

    Fix: Added new styles for Iphone SE

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import px2vw from "../../utils/px2vw";
+
 export const Main = styled.main`
     display: flex;
     align-items: center;
@@ -11,9 +13,14 @@ export const Content = styled.div`
     text-align: center;
     padding: 30px 20px;
     border: 3px solid #29EBAA;
-    width: 500px;
-    height: 640px;
+    width: ${px2vw(175,500)};
+    height: ${px2vw(250,600)};
     border-radius: 5px;
+
+    @media (max-width: 375px) {
+        width: ${px2vw(350,375)};
+        height: ${px2vw(505,375)};
+    }
 `;
 
 export const H1 = styled.h1`
@@ -21,7 +28,11 @@ export const H1 = styled.h1`
     font-family: monospace;
     font-size: 2rem;
     color: #29EBAA;
-    `;
+
+    @media (max-width: 375px) {
+        font-size: 3.2rem;
+    }    
+`;
 
 export const Form = styled.form`
 `;
@@ -34,4 +45,8 @@ export const labelError = styled.label`
     font-size: 14px;
     font-weight: bold;
     color: #29EBAA;
+
+    @media (max-width: 375px) {
+        font-size: 1.6rem;
+    }
 `;
